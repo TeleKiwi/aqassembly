2ac775033333df7c482949706d9b39e93f503348
TeleKiwi <70376250+TeleKiwi@users.noreply.github.com>
2025-07-04 00:10:44 +0100

    explain why mod.ts exists

--- src/lib/mod.ts
+++ src/lib/mod.ts
@@ -1,4 +1,5 @@
 // https://stackoverflow.com/a/17323608
+// This was added to allow negative numbers to be properly modulo-ed, which is useful for tests.
 
 export function mod(number: number, modulo: number) {
   return ((number % modulo) + modulo) % modulo;
